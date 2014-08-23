c5d04d9d58f48423c3279cf5643b1dc71b99e38f
Ben Bieker <mail@ben-bieker.de>
2014-08-23 13:02:17 +0200

    add string helper tests

--- string_helpers_test.go
+++ string_helpers_test.go
@@ -10,9 +10,11 @@ var _ = Describe("StringHelpers", func() {
 		Expect(underscorize("Post")).To(Equal("post"))
 		Expect(underscorize("post")).To(Equal("post"))
 		Expect(underscorize("SimplePost")).To(Equal("simple_post"))
+		Expect(underscorize("SimplePostComment")).To(Equal("simple_post_comment"))
 		Expect(underscorize("simple_post")).To(Equal("simple_post"))
 		Expect(underscorize("XML")).To(Equal("xml"))
 		Expect(underscorize("XMLPost")).To(Equal("xml_post"))
+		Expect(underscorize("XMLPostComment")).To(Equal("xml_post_comment"))
 	})
 
 	It("pluralizes", func() {
