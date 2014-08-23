e81680ab79a7cd9a0d661eef4de15e2e364ee303
Lucas Clemente <lucas@clemente.io>
2014-08-23 23:29:35 +0200

    camelize and singularize string helpers

--- string_helpers.go
+++ string_helpers.go
@@ -1,8 +1,9 @@
 package api2go
 
 import (
-	"bytes"
 	"regexp"
+	"strings"
+	"unicode"
 
 	"github.com/gedex/inflector"
 )
@@ -10,16 +11,47 @@ import (
 var (
 	underscorizeAbbreveationsRegex = regexp.MustCompile("([A-Z]+)([A-Z][a-z])")
 	underscorizeCamelCaseRegex     = regexp.MustCompile("([a-z\\d])([A-Z])")
+	camelizeRegex                  = regexp.MustCompile(`_[a-z\d]`)
+	camelizeAbbrevs = []string{
+		"xml",
+		"id",
+		"json",
+	}
 )
 
 // underscorize takes a camel-cased word and transforms it to a underscored version
 func underscorize(word string) string {
 	word = underscorizeAbbreveationsRegex.ReplaceAllString(word, "${1}_${2}")
 	word = underscorizeCamelCaseRegex.ReplaceAllString(word, "${1}_${2}")
-	return string(bytes.ToLower([]byte(word)))
+	return strings.ToLower(word)
+}
+
+//camelize takes a underscored word and transforms it to a camel-cased version
+func camelize(word string) string {
+	if word == "" {
+		return ""
+	}
+	// Special abbreviations
+	for _, v := range camelizeAbbrevs {
+		word = strings.Replace(word, v, strings.ToUpper(v), -1)
+	}
+	// Capitalize first char
+	rs := []rune(word)
+	rs[0] = unicode.ToUpper(rs[0])
+	word = string(rs)
+	// Replace rest
+	word = camelizeRegex.ReplaceAllStringFunc(word, func(w string) string {
+		return strings.ToUpper(w[1:])
+	})
+	return word
 }
 
 // pluralize a noun
 func pluralize(word string) string {
 	return inflector.Pluralize(word)
+}
+
+// singularize a noun
+func singularize(word string) string {
+	return inflector.Singularize(word)
 }
--- string_helpers_test.go
+++ string_helpers_test.go
@@ -10,16 +10,35 @@ var _ = Describe("StringHelpers", func() {
 		Expect(underscorize("Post")).To(Equal("post"))
 		Expect(underscorize("post")).To(Equal("post"))
 		Expect(underscorize("SimplePost")).To(Equal("simple_post"))
-		Expect(underscorize("SimplePostComment")).To(Equal("simple_post_comment"))
 		Expect(underscorize("simple_post")).To(Equal("simple_post"))
+		Expect(underscorize("SimplePostComment")).To(Equal("simple_post_comment"))
 		Expect(underscorize("XML")).To(Equal("xml"))
 		Expect(underscorize("XMLPost")).To(Equal("xml_post"))
 		Expect(underscorize("XMLPostComment")).To(Equal("xml_post_comment"))
+	})
+
+	It("camelizes", func() {
+		Expect(camelize("post")).To(Equal("Post"))
+		Expect(camelize("post")).To(Equal("Post"))
+		Expect(camelize("simple_post")).To(Equal("SimplePost"))
+		Expect(camelize("simple_post")).To(Equal("SimplePost"))
+		Expect(camelize("simple_post_comment")).To(Equal("SimplePostComment"))
+		Expect(camelize("xml")).To(Equal("XML"))
+		Expect(camelize("xml_post")).To(Equal("XMLPost"))
+		Expect(camelize("xml_post_comment")).To(Equal("XMLPostComment"))
+		Expect(camelize("id")).To(Equal("ID"))
+		Expect(camelize("json")).To(Equal("JSON"))
 	})
 
 	It("pluralizes", func() {
 		Expect(pluralize("post")).To(Equal("posts"))
 		Expect(pluralize("posts")).To(Equal("posts"))
 		Expect(pluralize("category")).To(Equal("categories"))
+	})
+
+	It("singularizes", func() {
+		Expect(singularize("posts")).To(Equal("post"))
+		Expect(singularize("post")).To(Equal("post"))
+		Expect(singularize("categories")).To(Equal("category"))
 	})
 })
