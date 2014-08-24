78fef785b350e72b54d40544800e6a684b09299f
Lucas Clemente <lucas@clemente.io>
2014-08-24 12:13:18 +0200

    implemented Unmarshal for simple objects

---
+++ unmarshal.go
@@ -0,0 +1,70 @@
+package api2go
+
+import (
+	"encoding/json"
+	"errors"
+	"reflect"
+)
+
+type unmarshalContext map[string]interface{}
+
+// Unmarshal reads a JSONAPI map to a model struct
+func Unmarshal(ctx unmarshalContext, values interface{}) error {
+	// Check that target is a *[]Model
+	ptrVal := reflect.ValueOf(values)
+	if ptrVal.Kind() != reflect.Ptr || ptrVal.IsNil() {
+		panic("You must pass a pointer to a []struct to Unmarshal()")
+	}
+	sliceType := reflect.TypeOf(values).Elem()
+	sliceVal := ptrVal.Elem()
+	if sliceType.Kind() != reflect.Slice {
+		panic("You must pass a pointer to a []struct to Unmarshal()")
+	}
+	structType := sliceType.Elem()
+	if structType.Kind() != reflect.Struct {
+		panic("You must pass a pointer to a []struct to Unmarshal()")
+	}
+
+	// Read models slice
+	rootName := pluralize(underscorize(structType.Name()))
+	var modelsInterface interface{}
+	if modelsInterface = ctx[rootName]; modelsInterface == nil {
+		return errors.New("Expected root document to include a '" + rootName + "' key but it didn't.")
+	}
+	models, ok := modelsInterface.([]interface{})
+	if !ok {
+		return errors.New("Expected slice under key '" + rootName + "'")
+	}
+
+	// Read all the models
+	for _, m := range models {
+		attributes, ok := m.(map[string]interface{})
+		if !ok {
+			return errors.New("Expected an array of objects under key '" + rootName + "'")
+		}
+
+		val := reflect.New(structType).Elem()
+		for k, v := range attributes {
+			camelizedKey := camelize(k)
+			field := val.FieldByName(camelizedKey)
+			if !field.IsValid() {
+				return errors.New("Expected struct " + structType.Name() + " to have field " + camelizedKey)
+			}
+			field.Set(reflect.ValueOf(v))
+		}
+
+		sliceVal.Set(reflect.Append(sliceVal, val))
+	}
+
+	return nil
+}
+
+// UnmarshalJSON reads a JSONAPI compatible JSON document to a model struct
+func UnmarshalJSON(data []byte, values interface{}) error {
+	var ctx unmarshalContext
+	err := json.Unmarshal(data, &ctx)
+	if err != nil {
+		return err
+	}
+	return Unmarshal(ctx, values)
+}
---
+++ unmarshal_test.go
@@ -0,0 +1,104 @@
+package api2go
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Unmarshal", func() {
+	Context("When unmarshaling simple objects", func() {
+		singleJSON := []byte(`{"simple_posts":[{"title":"First Post","text":"Lipsum"}]}`)
+		firstPost := SimplePost{Title: "First Post", Text: "Lipsum"}
+		secondPost := SimplePost{Title: "Second Post", Text: "Foobar!"}
+		singlePostMap := map[string]interface{}{
+			"simple_posts": []interface{}{
+				map[string]interface{}{
+					"title": firstPost.Title,
+					"text":  firstPost.Text,
+				},
+			},
+		}
+		multiplePostMap := map[string]interface{}{
+			"simple_posts": []interface{}{
+				map[string]interface{}{
+					"title": firstPost.Title,
+					"text":  firstPost.Text,
+				},
+				map[string]interface{}{
+					"title": secondPost.Title,
+					"text":  secondPost.Text,
+				},
+			},
+		}
+
+		It("unmarshals single objects", func() {
+			var posts []SimplePost
+			err := Unmarshal(singlePostMap, &posts)
+			Expect(err).To(BeNil())
+			Expect(posts).To(Equal([]SimplePost{firstPost}))
+		})
+
+		It("unmarshals multiple objects", func() {
+			var posts []SimplePost
+			err := Unmarshal(multiplePostMap, &posts)
+			Expect(err).To(BeNil())
+			Expect(posts).To(Equal([]SimplePost{firstPost, secondPost}))
+		})
+
+		It("panics on invalid params", func() {
+			Expect(func() {
+				Unmarshal(singlePostMap, nil)
+			}).To(Panic())
+			Expect(func() {
+				Unmarshal(singlePostMap, 42)
+			}).To(Panic())
+			Expect(func() {
+				Unmarshal(singlePostMap, []interface{}{})
+			}).To(Panic())
+			Expect(func() {
+				Unmarshal(singlePostMap, &[]interface{}{})
+			}).To(Panic())
+		})
+
+		It("errors on empty maps", func() {
+			var posts []SimplePost
+			err := Unmarshal(map[string]interface{}{}, &posts)
+			Expect(err).ToNot(BeNil())
+		})
+
+		It("errors on non-array root", func() {
+			var posts []SimplePost
+			err := Unmarshal(map[string]interface{}{
+				"simple_posts": 42,
+			}, &posts)
+			Expect(err).ToNot(BeNil())
+		})
+
+		It("errors on non-documents", func() {
+			var posts []SimplePost
+			err := Unmarshal(map[string]interface{}{
+				"simple_posts": []interface{}{42},
+			}, &posts)
+			Expect(err).ToNot(BeNil())
+		})
+
+		It("errors with wrong keys", func() {
+			var posts []SimplePost
+			err := Unmarshal(map[string]interface{}{
+				"simple_posts": []interface{}{
+					map[string]interface{}{
+						"foobar": 42,
+					},
+				},
+			}, &posts)
+			Expect(err).ToNot(BeNil())
+		})
+
+		It("unmarshals JSON", func() {
+			var posts []SimplePost
+			err := UnmarshalJSON(singleJSON, &posts)
+			Expect(err).To(BeNil())
+			Expect(posts).To(Equal([]SimplePost{firstPost}))
+		})
+	})
+})
