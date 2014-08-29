c8e1346279f94f4faa6a29d2b9baf0c54b03818a
Lucas Clemente <lucas@clemente.io>
2014-08-29 15:51:21 +0200

    implemented REST GET method

--- rest_handler_test.go
+++ rest_handler_test.go
@@ -24,19 +24,38 @@ var _ = Describe("RestHandler", func() {
 		)
 
 		BeforeEach(func() {
-			post1 = Post{Title: "Hello, World!"}
+			post1 = Post{ID: 1, Title: "Hello, World!"}
 			post1Map = map[string]interface{}{
-				"id":    "0",
+				"id":    "1",
 				"title": "Hello, World!",
 			}
 			rec = httptest.NewRecorder()
 			handler = HandlerForResource("posts", func() interface{} {
 				return []Post{post1}
+			}, func(id string) interface{} {
+				switch id {
+				case "1":
+					return post1
+				default:
+					panic("unknown id " + id)
+				}
 			})
 		})
 
 		It("GETs collections", func() {
 			req, err := http.NewRequest("GET", "/posts", nil)
+			Expect(err).To(BeNil())
+			handler.ServeHTTP(rec, req)
+			Expect(rec.Code).To(Equal(http.StatusOK))
+			var result map[string]interface{}
+			Expect(json.Unmarshal(rec.Body.Bytes(), &result)).To(BeNil())
+			Expect(result).To(Equal(map[string]interface{}{
+				"posts": []interface{}{post1Map},
+			}))
+		})
+
+		It("GETs single objects", func() {
+			req, err := http.NewRequest("GET", "/posts/1", nil)
 			Expect(err).To(BeNil())
 			handler.ServeHTTP(rec, req)
 			Expect(rec.Code).To(Equal(http.StatusOK))
