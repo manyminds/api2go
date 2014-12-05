013399078c5bb8db4e974971f59339fa2b28d477
Ben Bieker <mail@ben-bieker.de>
2014-12-05 10:48:31 +0100

    discard log messages in tests to keep output clean

--- api2go_suite_test.go
+++ api2go_suite_test.go
@@ -3,11 +3,14 @@ package api2go
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"io/ioutil"
+	"log"
 
 	"testing"
 )
 
 func TestApi2go(t *testing.T) {
 	RegisterFailHandler(Fail)
+	log.SetOutput(ioutil.Discard)
 	RunSpecs(t, "Api2go Suite")
 }
