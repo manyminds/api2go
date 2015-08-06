b90f49ff47251c0e97512504bb0129760b7418c3
n.wagensonner <n.wagensonner@manyminds.de>
2015-08-06 21:02:13 +0200

    Test that wrong registration panics

--- api_test.go
+++ api_test.go
@@ -16,6 +16,12 @@ import (
 	"gopkg.in/guregu/null.v2"
 )
 
+type invalid string
+
+func (i invalid) GetID() string {
+	return "invalid"
+}
+
 type Post struct {
 	ID       string `json:"-"`
 	Title    string
@@ -1411,6 +1417,18 @@ var _ = Describe("RestHandler", func() {
 				Expect(rec.Body.String()).To(MatchJSON(expected))
 				Expect(rec.Header().Get("Content-Type")).To(Equal(defaultContentTypHeader))
 				Expect(rec.Code).To(Equal(http.StatusMethodNotAllowed))
+			})
+		})
+
+		Context("add resource panics with invalid resources", func() {
+			It("Should really panic", func() {
+				api := NewAPI("blub")
+				invalidDataStructure := new(invalid)
+				testFunc := func() {
+					api.AddResource(*invalidDataStructure, &userSource{})
+				}
+
+				Expect(testFunc).To(Panic())
 			})
 		})
 
