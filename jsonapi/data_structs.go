d56d463ef468340cd5e51f2ca36a4c1ca45617b3
Ben Bieker <mail@ben-bieker.de>
2015-10-13 10:35:55 +0200

    Structs that represent a jsonapi document
    It is now possible to marshal/unmarshal into these more general structs that
    represent the structure of a jsonapi compatible document. Therefore this is
    more standards compatible to the Marshal and Unmarshal methods of the Go json
    package

---
+++ jsonapi/data_structs.go
@@ -0,0 +1,92 @@
+package jsonapi
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+)
+
+// Document represents a JSONAPI document like specified: http://jsonapi.org
+type Document struct {
+	Links    Links         `json:"links,omitempty"`
+	Data     DataContainer `json:"data"`
+	Included []Data        `json:"included,omitempty"`
+}
+
+// DataContainer is needed to either keep "data" contents as array or object.
+type DataContainer struct {
+	IsArray    bool
+	DataObject Data
+	DataArray  []Data
+}
+
+// UnmarshalJSON implements Unmarshaler because we have to detect if payload is an object
+// or an array
+func (c *DataContainer) UnmarshalJSON(payload []byte) error {
+	if bytes.HasPrefix(payload, []byte("{")) {
+		// payload is an object
+		return json.Unmarshal(payload, &c.DataObject)
+	}
+
+	if bytes.HasPrefix(payload, []byte("[")) {
+		// payload is an array
+		c.IsArray = true
+		return json.Unmarshal(payload, &c.DataArray)
+	}
+
+	return errors.New("Invalid json for data array/object")
+}
+
+// Links is general links struct for top level and relationships
+type Links struct {
+	Self     string `json:"self,omitempty"`
+	Related  string `json:"related,omitempty"`
+	First    string `json:"first,omitempty"`
+	Previous string `json:"previous,omitempty"`
+	Next     string `json:"next,omitempty"`
+	Last     string `json:"last,omitempty"`
+}
+
+// Data for top level and included data
+type Data struct {
+	Type          string                  `json:"type"`
+	ID            string                  `json:"id"`
+	Attributes    json.RawMessage         `json:"attributes"`
+	Relationships map[string]Relationship `json:"relationships,omitempty"`
+	Links         Links                   `json:"links,omitempty"`
+}
+
+// Relationship contains reference IDs to the related structs
+type Relationship struct {
+	Links Links                     `json:"links,omitempty"`
+	Data  RelationshipDataContainer `json:"data,omitempty"`
+}
+
+// RelationshipDataContainer is needed to either keep relationship "data" contents as array or object.
+type RelationshipDataContainer struct {
+	IsArray    bool
+	DataObject RelationshipData
+	DataArray  []RelationshipData
+}
+
+// UnmarshalJSON implements Unmarshaler and also detects array/object type
+func (c *RelationshipDataContainer) UnmarshalJSON(payload []byte) error {
+	if bytes.HasPrefix(payload, []byte("{")) {
+		// payload is an object
+		return json.Unmarshal(payload, &c.DataObject)
+	}
+
+	if bytes.HasPrefix(payload, []byte("[")) {
+		// payload is an array
+		c.IsArray = true
+		return json.Unmarshal(payload, &c.DataArray)
+	}
+
+	return errors.New("Invalid json for relationship data array/object")
+}
+
+// RelationshipData represents one specific reference ID
+type RelationshipData struct {
+	Type string `json:"type"`
+	ID   string `json:"id"`
+}
---
+++ jsonapi/data_structs_test.go
@@ -0,0 +1,102 @@
+package jsonapi
+
+import (
+	"encoding/json"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("JSONAPI Struct tests", func() {
+	Context("Testing array and object data payload", func() {
+		It("detects object payload", func() {
+			sampleJSON := `
+			{
+				"data":
+				{
+					"type": "test",
+					"id": "1",
+					"attributes": {"foo": "bar"},
+					"relationships": {
+						"author": {
+							"data": {"type": "author", "id": "1"}
+						}
+					}
+				}
+			}
+`
+			expectedData := Data{
+				Type:       "test",
+				ID:         "1",
+				Attributes: json.RawMessage([]byte(`{"foo": "bar"}`)),
+				Relationships: map[string]Relationship{
+					"author": {
+						Data: RelationshipDataContainer{
+							IsArray: false,
+							DataObject: RelationshipData{
+								Type: "author",
+								ID:   "1",
+							},
+						},
+					},
+				},
+			}
+
+			target := Document{}
+
+			err := json.Unmarshal([]byte(sampleJSON), &target)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(target.Data.DataObject).To(Equal(expectedData))
+		})
+
+		It("detects array payload", func() {
+			sampleJSON := `
+			{
+				"data": [
+					{
+						"type": "test",
+						"id": "1",
+						"attributes": {"foo": "bar"},
+						"relationships": {
+							"comments": {
+								"data": [
+									{"type": "comments", "id": "1"},
+									{"type": "comments", "id": "2"}
+								]
+							}
+						}
+					}
+				]
+			}
+`
+			expectedData := Data{
+				Type:       "test",
+				ID:         "1",
+				Attributes: json.RawMessage([]byte(`{"foo": "bar"}`)),
+				Relationships: map[string]Relationship{
+					"comments": {
+						Data: RelationshipDataContainer{
+							IsArray: true,
+							DataArray: []RelationshipData{
+								{
+									Type: "comments",
+									ID:   "1",
+								},
+								{
+									Type: "comments",
+									ID:   "2",
+								},
+							},
+						},
+					},
+				},
+			}
+
+			target := Document{}
+
+			err := json.Unmarshal([]byte(sampleJSON), &target)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(target.Data.DataArray).To(Equal([]Data{expectedData}))
+		})
+	})
+})
